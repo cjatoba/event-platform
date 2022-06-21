84c27afbb9e0f7d8e0bec4110673746cfafcbad6
cjatoba <claytonjatoba@gmail.com>
2022-06-21 10:15:01 -0300

    Aula 1 • O início da especialização em ReactJS

---
+++ src/lib/apollo.ts
@@ -0,0 +1,6 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+    uri: "https://api-sa-east-1.graphcms.com/v2/cl4o4ycmg0lbb01z7fczv83tf/master",
+    cache: new InMemoryCache(),
+})
